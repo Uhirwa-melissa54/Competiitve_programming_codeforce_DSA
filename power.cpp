75806327946c701eb552de665c1e69b7ec9a14cb
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-20 22:46:33 +0200

    solved Bear and Big Brother

---
+++ power.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int power(int n,int m){
+	if(m==0)
+		return 1;
+	else 
+	return n* power(n,m-1);	
+	
+	
+}
+int main(){
+	int n,m;
+	cin >>n>>m;
+	int result=power(n,m);
+	cout <<result;
+}
